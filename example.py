cc1bd714a59fa566976e3af1c90ccc031445cc59
Søren Rasmussen <soren.rasmussen@alexandra.dk>
2018-05-16 10:55:41 +0200

    Added example

---
+++ example.py
@@ -0,0 +1,15 @@
+import cv2
+import streamserver
+cap = cv2.VideoCapture(0)
+
+ret,_ = cap.read()
+assert ret == True
+
+with streamserver.StreamServer(quality=75,host='localhost',port=5000) as ss:
+    while cap.isOpened():
+        ret,frame = cap.read()
+        ss.set_frame(frame)
+        wk = cv2.waitKey(20)
+        if wk == ord('q'):
+            break
+cap.release()
