f1da4edb1c94662b5145d7679741670d58d0a8f3
cnheider <mrnaah@gmail.com>
2019-08-29 13:17:08 +0200

    Made samples folder

---
+++ samples/ss_imageio.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from contextlib import suppress
+
+from imageio.core import Format
+
+__author__ = "cnheider"
+__doc__ = r"""Broken at the moment"""
+
+
+def main():
+    import imageio
+    import streamserver
+
+    reader: Format.Reader = imageio.get_reader("<video0>")
+
+    with suppress(KeyboardInterrupt):
+        with streamserver.StreamServer(
+            JPEG_quality=75, host="localhost", port=5000
+        ) as ss:
+            for im in reader:
+                ss.set_frame(im)
+
+    reader.close()
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ python_version_check()
 import pathlib
 import re
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open(
     pathlib.Path(__file__).parent / "streamserver" / "__init__.py", "r"
@@ -23,13 +23,72 @@ with open(
         r"PROJECT_NAME = ['\"]([^'\"]*)['\"]", content, re.M
     ).group(1)
 
+
+def get_requirements():
+    requirements_out = []
+    with open("requirements.txt") as f:
+        requirements = f.readlines()
+
+        for requirement in requirements:
+            requirements_out.append(requirement.strip())
+
+    return requirements_out
+
+
+def get_entry_points():
+    return {
+        "console_scripts": [
+            "ss-cv2 = streamserver.entry_points.ss_cv2:main",
+            "ss-imageio = streamserver.entry_points.ss_imageio:main",
+        ]
+    }
+
+
+def get_readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+def get_keyword():
+    with open("KEYWORDS.md") as f:
+        return f.read()
+
+
+def get_license():
+    return "Apache License, Version 2.0"
+
+
+def get_classifiers():
+    return [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English",
+    ]
+
+
 setup(
     name=project_name,
     version=version,
     description="Server to stream multi-part images over HTTP",
     author="Soeren Rasmussen",
-    packages=["streamserver"],
-    install_requires=["numpy", "imageio"],
     package_data={"": ["viewer_mini.html"]},
     include_package_data=True,
+    python_requires=">=3",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    entry_points=get_entry_points(),
+    license=get_license(),
+    classifiers=get_classifiers(),
+    long_description_content_type="text/markdown",
+    long_description=get_readme(),
+    url="",
+    download_url="",
 )
