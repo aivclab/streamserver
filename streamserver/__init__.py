4dc55cfd497a7d098fe023eaf0d0807d9421c92d
cnheider <mrnaah@gmail.com>
2020-03-27 23:31:51 +0100

    The cool branch

--- streamserver/__init__.py
+++ streamserver/__init__.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 from apppath import AppPath
 
+__project__ = "StreamServer"
 __author__ = "Soeren Rasmussen"
 __version__ = "0.4.2"
 __doc__ = r"""
           .. module:: streamserver
              :platform: Unix, Windows
              :synopsis: multipart image HTTP streaming server.
-          
+
           .. moduleauthor:: Soeren Rasmussen
-          
+
           Created on 27/04/2018
-          
+
           @author: Soeren Rasmussen
           """
 
-PROJECT_NAME = "StreamServer"
+PROJECT_NAME = __project__.lower().strip().replace(" ", "_")
 PROJECT_AUTHOR = __author__.lower().strip().replace(" ", "_")
 PROJECT_APP_PATH = AppPath(app_name=PROJECT_NAME, app_author=PROJECT_AUTHOR)
--- streamserver/samples/ss_cv2.py
+++ streamserver/samples/ss_cv2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-__author__ = 'cnheider'
+__author__ = "cnheider"
 
 
 def main():
@@ -12,15 +12,15 @@ def main():
     ret, _ = cap.read()
     assert ret == True
 
-    with streamserver.StreamServer(JPEG_quality=75, host='localhost', port=5000) as ss:
+    with streamserver.StreamServer(JPEG_quality=75, host="localhost", port=5000) as ss:
         while cap.isOpened():
             ret, frame = cap.read()
             ss.set_frame(frame)
             wk = cv2.waitKey(20)
-            if wk == ord('q'):
+            if wk == ord("q"):
                 break
     cap.release()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
